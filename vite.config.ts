9e10f5cc7cd6a2e17f292ddec55f86c6226b57c9
daabarbacena <danielbtm@outlook.com>
2026-01-14 21:40:30 -0300

    fix 404.5

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
-import { mochaPlugins } from "@getmocha/vite-plugins";
 
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  plugins: [...mochaPlugins(process.env as any), react(), cloudflare()],
+  plugins: [react()],
   server: {
     allowedHosts: true,
   },
   build: {
+    outDir: "dist",
     chunkSizeWarningLimit: 5000,
   },
   resolve: {
